cc42af28f899d9cfb28280cdf100e13768c3611f
dkrzoska <d.krzoska@gmail.com>
2020-10-27 22:49:37 +0100

    fresh start

--- js/app.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Application from "./Components/Application/Application";
-
-function App() {
-  return <Application />
-}
-
-ReactDOM.render(<App/>, document.querySelector("#app"));
